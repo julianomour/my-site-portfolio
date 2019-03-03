d370a66763705036115905259d599afa3141e105
Juliano Moura <julianomoura05@gmail.com>
2019-03-03 13:32:32 -0300

    fix error in server js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
 
-app.use(express.static(__dirname + "/dist"));
+app.use(express.static(__dirname + "/dist/my-site"));
 app.listen(process.env.PORT || 8080);
 
